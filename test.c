521765b37ecedeec351ff7c15789cbdd429c035b
thomas <thomvray@gmail.com>
2023-11-29 13:26:45 +0100

    string can be stored

--- test.c
+++ test.c
@@ -8,7 +8,7 @@ PG_MODULE_MAGIC;
 typedef struct {
     int32 length;
     char data[FLEXIBLE_ARRAY_MEMBER];
-} text;
+} test;
 
 PG_FUNCTION_INFO_V1(test_in);
 PG_FUNCTION_INFO_V1(test_out);
@@ -21,11 +21,11 @@ test_in(PG_FUNCTION_ARGS)
     {
         ereport(ERROR, errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED), errmsg("null input not allowed!!!!!"));
     } */
-    text *destination = (text *) palloc(VARHDRSZ + strlen(input_str));
+    test *destination = (test *) palloc(VARHDRSZ + strlen(input_str));
 	SET_VARSIZE(destination, VARHDRSZ + strlen(input_str));
 	memcpy(destination->data, input_str, strlen(input_str));
 
-    PG_RETURN_POINTER(result);
+    PG_RETURN_POINTER(destination);
 }
 
 Datum
@@ -33,5 +33,5 @@ test_out(PG_FUNCTION_ARGS)
 {
     test *input = (test *)PG_GETARG_POINTER(0);
 
-    PG_RETURN_CSTRING(text_to_cstring(input->data));
+    PG_RETURN_CSTRING(input->data);
 }
